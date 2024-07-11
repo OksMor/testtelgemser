2034116bf1c1720d3b914894270fd68eade83f70
OksMor <Sancho_2006_ru@mail.ru>
2024-07-11 15:46:37 +0300

    last

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const TelegramBot = require("node-telegram-bot-api");
-const TOKEN = "7477985126:AAEd8R0OEsCe13sPuQjyLrhNXDeEY4N7OSM";
+const TOKEN = "TOKEN";
 const server = express();
 const bot = new TelegramBot(TOKEN, {
     polling: true
